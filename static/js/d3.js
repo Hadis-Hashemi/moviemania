20df9eef4d13b22815e46dfe0b593a1819722632
Mitch Klee <mithcklee35@gmail.com>
2020-10-22 09:58:50 -0500

    updating html format with tabs

--- static/js/d3.js
+++ static/js/d3.js
@@ -1,25 +1,25 @@
 console.log(d3)
 var tbody = d3.select("tbody")
-d3.json("/api/v1.0/imdb_data",{method:"GET"})
-.then(function(response){
-    // console.log(response)
-    var imdbData = response.slice(0,10);
+d3.json("/api/v1.0/imdb_data", { method: "GET" })
+    .then(function (response) {
+        // console.log(response)
+        var imdbData = response.slice(0, 10);
 
-    imdbData.forEach(function(imdb) {
-        console.log(imdb);
-        // Append each row with the data
-        var row = tbody.append("tr");
+        imdbData.forEach(function (imdb) {
+            console.log(imdb);
+            // Append each row with the data
+            var row = tbody.append("tr");
 
-        Object.entries(imdb).forEach(function([key, value]) {
-            console.log(key, value);
-            // Append each value to the corresponding cell
-            var cell = row.append("td");
-            cell.text(value);
+            Object.entries(imdb).forEach(function ([key, value]) {
+                console.log(key, value);
+                // Append each value to the corresponding cell
+                var cell = row.append("td");
+                cell.text(value);
+            });
         });
     });
-});
 var button = d3.select("#filter-btn");
-button.on("click", function() {
+button.on("click", function () {
     tbody.html("");
 
     var inputElement = d3.select("#datetime");
@@ -27,32 +27,58 @@ button.on("click", function() {
     var inputValue = inputElement.property("value");
     console.log(inputValue);
     d3.json("/api/v1.0/flickerpicker", {
-        method:"POST",
-        body:JSON.stringify({inputValue}),
-        headers:{
+        method: "POST",
+        body: JSON.stringify({ inputValue }),
+        headers: {
             "Content-Type": "application/json"
         }
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
         message = "You may like these movies ";
-        if (response[0]!= null){
-            message = message+ response[0]
+        if (response[0] != null) {
+            message = message + response[0]
         }
-        alert (message)
+        alert(message)
         var flickerData = response
-    }).catch(function(error){console.log(error)});
+    }).catch(function (error) { console.log(error) });
 
 
-//     filteredData.forEach(function(selections) {
+    //     filteredData.forEach(function(selections) {
 
-//     console.log(selections);
-//     // Append one table row 
-//     var row = tbody.append("tr");
-//     // Use `Object.entries` to print each UFO Sighting value
-//     Object.entries(selections).forEach(function([key, value]) {
-//         console.log(key, value);
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
+    //     console.log(selections);
+    //     // Append one table row 
+    //     var row = tbody.append("tr");
+    //     // Use `Object.entries` to print each UFO Sighting value
+    //     Object.entries(selections).forEach(function([key, value]) {
+    //         console.log(key, value);
+    //         var cell = row.append("td");
+    //         cell.text(value);
+    //     });
+    // });
 });
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+// window.onload=function(){
+//     document.getElementById("choose").click();
+//   };
